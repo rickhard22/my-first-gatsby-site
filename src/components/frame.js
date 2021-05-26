621f836a39700dd8fbf4eba16d3cd244e13280a8
Ricardo de Sousa Vieira <ricardo.vieira@chaserhq.com>
2021-05-26 18:59:13 +0100

    Using styled-components

--- src/components/frame.js
+++ src/components/frame.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Title = styled.button`
+  /* Adapt based on primary prop */
+  background: ${props => (props.primary ? 'black' : 'white')};
+  color: ${props => (props.primary ? 'white' : 'black')};
+
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
 
 const Frame = ({ children }) => {
   return (
     <div>
-      <h1>This is the page title</h1>
+      <Title primary>This is the page title</Title>
       { children }
     </div>
   )
